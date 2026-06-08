3b30dbd0de10b2c1d5ff0fdd51297490df7d4338
xieyi07 <xieyi07@kuaishou.com>
2026-06-08 17:38:42 +0800

    feat: 文件处理工具

--- src-tauri/src/path/mod.rs
+++ src-tauri/src/path/mod.rs
@@ -1,7 +1,9 @@
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use directories::ProjectDirs;
 use tauri::{AppHandle, Manager};
-use crate::context::session_context::SessionContext;
+use std::fs::{read_to_string, create_dir_all, write, exists, OpenOptions, remove_file};
+use anyhow::anyhow;
 
 pub static QUALIFIER: &str ="com";
 pub static ORGANIZATION: &str ="ThousandDream";
@@ -10,17 +12,38 @@ pub static APPLICATION: &str ="simple-agent";
 pub fn init_data_dir(app:&AppHandle)->anyhow::Result<()>{
      let dir = app.path().app_data_dir()?;
      println!("数据目录：{:?}", dir);
+     // println!("库处理结果:{:?}",ProjectDirs::from(QUALIFIER,ORGANIZATION,APPLICATION).unwrap().data_dir().to_str().unwrap()); //这个值和上面的dir相等，所以后面处理直接用tauri提供的包就好
      std::fs::create_dir_all(&dir)?;
      Ok(())
 }
-// pub fn write_file(file_path: &str, contents: String)->anyhow::Result<bool>{
-//      let data_dir=ProjectDirs::from(QUALIFIER,ORGANIZATION,APPLICATION);
-//      let data_path = PathBuf::from()
-// }
-pub fn add_file(){
+pub fn write_file(app:&AppHandle,file_path: &str, contents: String)->anyhow::Result<bool>{
+     let dir = app.path().app_data_dir()?;
+     let file_path = dir.join(file_path);
+
+     if exists(&file_path).is_ok() {
+          write(&file_path, contents)?
+     }else {
+          create_dir_all(&file_path)?;
+          write(&file_path, contents)?;
+     };
+     Ok(true)
 
 }
-pub fn delete_file(){
-
+pub fn add_file(app:&AppHandle,file_path: &str, contents: String)->anyhow::Result<bool>{
+     let dir = app.path().app_data_dir()?;
+     let file_path = dir.join(file_path);
+     let mut file = OpenOptions::new().create(true).append(true).open(&file_path)?;
+     writeln!(file, "{}", contents)?;
+     Ok(true)
+}
+pub fn delete_file(app:&AppHandle,file_path: &str, contents: String)->anyhow::Result<bool>{
+     let dir = app.path().app_data_dir()?;
+     let file_path = dir.join(file_path);
+     if exists(&file_path).is_ok() {
+          remove_file(file_path)?;
+     }else{
+          return Err(anyhow!("文件不存在"));
+     }
+     Ok(true)
 }
 
